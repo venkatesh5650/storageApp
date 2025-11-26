1ae59316290e833c03b89fb9fccda08535d5e94d
Karthan Venkatesh <karthanvenkateshvenkatesh@gmail.com>
2025-11-26 21:07:37 +0530

    fixed date issue

--- storageApp/backend/src/models/Folder.js
+++ storageApp/backend/src/models/Folder.js
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
 
-const folderSchema = new mongoose.Schema({
-  name: String,
-  parent: { type: mongoose.Schema.Types.ObjectId, ref: "Folder", default: null },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
-});
+const folderSchema = new mongoose.Schema(
+  {
+    name: String,
+    parent: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Folder",
+      default: null,
+    },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true } 
+);
 
 export default mongoose.model("Folder", folderSchema);
