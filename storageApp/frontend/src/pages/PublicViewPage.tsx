1b9f888b0a13eb0e122ed147971d7c97cb7f245c
venkatesh_karthan <karthanvenkateshvenkatesh@gmail.com>
2025-12-07 09:45:56 +0530

    Replace api import with publicApi import

--- storageApp/frontend/src/pages/PublicViewPage.tsx
+++ storageApp/frontend/src/pages/PublicViewPage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import api from "../api/client";
+import publicApi from "../api/publicApi";
+
 
 interface Folder {
   _id: string;
